52cfecbb564d9b1d30ee9c75c79fad6cf45ad549
matcharr <mathis.charretier.42@gmail.com>
2021-09-28 19:17:21 +0000

    SmallCard responsive done

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Header from "../components/Header"
 import Banner from "../components/Banner"
+import SmallCard from "../components/SmallCard"
+import MediumCard from "../components/MeduimCard"
 
-export default function Home() {
+export default function Home({exploreData}) {
+
   return (
     <div className="">
       <Head>
@@ -17,8 +20,42 @@ export default function Home() {
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           {/* Pull some data from a server - API endpoint*/}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({img, distance, location}) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}/>
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          {cardsData?.map(({}) => (
+            <MediumCard />
+          ))}
         </section>
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+      (res) => res.json()
+    );
+
+  const cardsData = await fetch('https://links.papareact.com/zp1').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData
+    }
+  };
 }
